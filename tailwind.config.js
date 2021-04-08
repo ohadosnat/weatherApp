f1d7c18ffb8cd492afe90eb8fa5fa99473bdc028
Ohad <ohadosnatcv@gmail.com>
2021-04-09 00:45:27 +0300

    html & css structure done for now. JS is next

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      textColor: {
+        skin: {
+          base: 'var(--color-text-base)',
+          forecast: () => {
+            return `rgb(var(--color-forecast))`
+          }
+        },
+      },
+      gradientColorStops: {
+        skin: {
+          start: 'var(--color-fill)',
+          middle: 'var(--color-via)',
+          end: 'var(--color-to)'
+        }
+      },
+      backgroundColor: {
+        skin: {
+          forecast: ({ opacityValue }) => {
+            if (opacityValue != undefined) {
+              return `rgba(var(--color-forecast), ${opacityValue})`
+            }
+            return `rgb(var(--color-forecast))`
+          },
+          divider: 'var(--color-text-base)'
+        }
+      },
+      fontFamily: {
+        'merriweather': ['Merriweather, serif'],
+        'merriweather-sans': ['Merriweather Sans, sans-serif'],
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+}
