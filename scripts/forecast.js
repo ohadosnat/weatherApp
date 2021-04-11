e14611cc740613b4a988c8776d791f990cc5ae94
Ohad <ohadosnatcv@gmail.com>
2021-04-11 11:43:28 +0300

    removed url parameter

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,11 +1,10 @@
-const url = "https://thebeautifultauren.github.io/weatherApp/";
-const key = "dX5RtZ13ChPmqWBeXAf70WAneDdDLeCR"; // api key - if requests gets to limit, open a new "app" and replace the key.
+const key = "qAbwxpUG1d5BMxe3WtRl923DsBLI9UPP"; // api key - if requests gets to limit, open a new "app" and replace the key.
 
 const getWeather = async (id) => {
     const base = "http://dataservice.accuweather.com/currentconditions/v1/";
     const query = `${id}?apikey=${key}`;
 
-    const response = await fetch(url + base + query);
+    const response = await fetch(base + query);
     const data = await response.json();
     return data[0];
 };
@@ -14,7 +13,7 @@ const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
-    const response = await fetch(url + base + query);
+    const response = await fetch(base + query);
     const data = await response.json();
 
     return data[0];
@@ -24,7 +23,7 @@ const getForecast12hours = async (id) => {
     const base = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/";
     const query = `${id}?apikey=${key}`;
 
-    const response = await fetch(url + base + query);
+    const response = await fetch(base + query);
     const data = await response.json();
 
     return data;
@@ -33,7 +32,7 @@ const getForecast5days = async (id) => {
     const base = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/";
     const query = `${id}?apikey=${key}`;
 
-    const response = await fetch(url + base + query);
+    const response = await fetch(base + query);
     const data = await response.json();
 
     return data;
